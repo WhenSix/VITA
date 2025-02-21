17494a4bd3236ffb95cd43cd9091386f0ea2373f
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2025-02-21 20:02:05 -0300

    sql: adiciona script de criação do banco de dados

---
+++ sql/database_script.sql
@@ -0,0 +1,48 @@
+USE whensix;
+
+CREATE TABLE tb_log (
+    id_log INT PRIMARY KEY AUTO_INCREMENT,
+    msg_log VARCHAR(45),
+    dt_log DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tb_endereco (
+    id_endereco INT PRIMARY KEY AUTO_INCREMENT,
+    CEP CHAR(8) NOT NULL,
+    logradouro_endereco VARCHAR(100) NOT NULL,
+    bairro_endereco VARCHAR(45) NOT NULL,
+    numero_endereco VARCHAR(45),
+    complemento_endereco VARCHAR(100)
+);
+
+CREATE TABLE tb_tipo_empresa (
+    id_tipo INT PRIMARY KEY AUTO_INCREMENT,
+    nome_tipo VARCHAR(45) NOT NULL,
+    descricao_tipo VARCHAR(100)
+);
+
+CREATE TABLE tb_empresa (
+    id_empresa INT PRIMARY KEY AUTO_INCREMENT,
+    razao_social_empresa VARCHAR(45) NOT NULL,
+    cnpj_empresa VARCHAR(45) NOT NULL UNIQUE,
+    nome_fantasia_empresa VARCHAR(45),
+    dt_criacao_empresa DATE,
+    fk_tipo_empresa INT,
+    fk_endereco_empresa INT,
+    FOREIGN KEY (fk_tipo_empresa) REFERENCES tb_tipo_empresa(id_tipo),
+    FOREIGN KEY (fk_endereco_empresa) REFERENCES tb_endereco(id_endereco)
+);
+
+CREATE TABLE tb_usuario (
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(45) NOT NULL,
+    email_usuario VARCHAR(200) NOT NULL UNIQUE,
+    telefone_usuario CHAR(11),
+    dt_nasc_usuario DATE,
+    dt_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    login_usuario VARCHAR(45) NOT NULL UNIQUE,
+    senha_usuario VARCHAR(255) NOT NULL, -- Armazene o hash da senha aqui
+    master_usuario BOOLEAN DEFAULT FALSE,
+    fk_empresa INT,
+    FOREIGN KEY (fk_empresa) REFERENCES tb_empresa(id_empresa)
+);
