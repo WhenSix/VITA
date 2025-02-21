afc57645b8ffd6480c3f19074824ee2220579aee
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2025-02-21 20:43:45 -0300

    add: adicionado projeto java com Log funcionando

---
+++ whensix_projeto/src/main/java/sptech/whensix/Main.java
@@ -0,0 +1,9 @@
+package sptech.whensix;
+
+public class Main {
+    public static void main(String[] args) {
+        Log log = new Log();
+        String[] proccessList = {"processo1", "processo2", "processo3"};
+        log.getLog(proccessList);
+    }
+}
