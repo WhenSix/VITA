8892621939f9c49facf42fe6054cdc430fc8ea78
cainaGomesDS <caina.silva@sptech.school>
2025-04-14 17:35:28 -0300

    feature: Adicionando os logs de leitura

--- whensix_projeto/src/main/java/sptech/whensix/Main.java
+++ whensix_projeto/src/main/java/sptech/whensix/Main.java
@@ -1,31 +1,37 @@
 package sptech.whensix;
 
-import sptech.whensix.logs.Log;
 import sptech.whensix.s3.S3Downloader;
 import sptech.whensix.excel.ExcelLeitor;
-
 import java.io.File;
+import sptech.whensix.utils.*;
+import sptech.whensix.excel.ResultadoLeitura;
 
 public class Main {
-//    public static void main(String[] args) {
+    //    public static void main(String[] args) {
 //        Log log = new Log();
 //        String[] proccessList = {"processo1", "processo2", "processo3"};
 //        log.getLog(proccessList);
 //    }
 //
-public static void main(String[] args) {
-    try {
-        String nomeArquivoS3 = "exemplo.xlsx"; // nome no bucket
-        String caminhoLocal = "arquivo-baixado.xlsx"; // salvar localmente
+    public static void main(String[] args) {
+        ResultadoLeitura resultado = new ResultadoLeitura(0,0);
 
-        File arquivo = S3Downloader.baixarArquivo(nomeArquivoS3, caminhoLocal);
-        System.out.println("Arquivo baixado: " + arquivo.getAbsolutePath());
+        try {
+            CreateLog.log(NivelLog.INFO, TipoLog.READ_START);
 
-        ExcelLeitor.processar(arquivo);
+            String nomeArquivoS3 = "Vigitel-2023-peso-rake.xlsx"; // nome no bucket
+            String caminhoLocal = "Documentos/Vigitel-2023-peso-rake.xlsx"; // salvar localmente
+            //S3Downloader.baixarArquivo(nomeArquivoS3, caminhoLocal);
+            File arquivo = new File("C:/Users/caina/Documents/dados_pi/Vigitel-2023-peso-rake-Copia.xlsx");
+            System.out.println("Arquivo baixado: " + arquivo.getAbsolutePath());
 
-    } catch (Exception e) {
-        e.printStackTrace();
+            resultado = ExcelLeitor.processar(arquivo);
+        } catch (Exception e) {
+            CreateLog.logCustom(NivelLog.ERROR,TipoLog.READ_ERROR, resultado.getSucessos(), resultado.getErros());
+
+            e.printStackTrace();
+        }
+        CreateLog.logCustom(NivelLog.INFO, TipoLog.READ_SUCESS, resultado.getSucessos(), resultado.getErros());
     }
-}
 
 }
